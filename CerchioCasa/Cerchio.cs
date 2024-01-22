97e5a5bac3acdecc61692e36a2fa738e0eb2607d
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-22 20:15:45 +0100

    Aggiungere i file di progetto.

---
+++ CerchioCasa/Cerchio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioCasa
+{
+    internal class Cerchio
+    {
+        public double Raggio { get; set; }
+
+        public Cerchio()
+        {
+
+        }
+
+        public double Calcolap()
+        {
+            return 2 * Math.PI * Raggio;
+        }
+
+        public double Calcolaa()
+        {
+            return Raggio * Raggio * Math.PI;
+        }
+    }
+}
---
+++ CerchioCasa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioCasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c = new Cerchio();
+            Console.WriteLine("Inserisci il raggio:");
+            c.Raggio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Il perimetro è:{0}, l'area è:{1}", c.Calcolap(), c.Calcolaa());
+
+            Console.ReadLine();
+        }
+    }
+}
